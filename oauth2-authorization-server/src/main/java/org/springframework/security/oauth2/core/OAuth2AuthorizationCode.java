c2931122107c3e9475a5b62743546263254386c8
kaka <1079914652@qq.com>
2021-09-18 17:54:31 +0800

    test

--- oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/core/OAuth2AuthorizationCode.java
+++ oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/core/OAuth2AuthorizationCode.java
@@ -31,7 +31,7 @@ public class OAuth2AuthorizationCode extends AbstractOAuth2Token {
 	/**
 	 * Constructs an {@code OAuth2AuthorizationCode} using the provided parameters.
 	 * @param tokenValue the token value
-	 * @param issuedAt the time at which the token was issued, hello
+	 * @param issuedAt the time at which the token was issued
 	 * @param expiresAt the time at which the token expires
 	 */
 	public OAuth2AuthorizationCode(String tokenValue, Instant issuedAt, Instant expiresAt) {
